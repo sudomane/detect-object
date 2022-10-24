4cdadec8f9a96bf43c6db3c44a0d12adecd9669a
philippe.bouchet <philippe.bouchet@epita.fr>
2022-10-24 21:42:54 +0200

    Tests now depend on the output of previous tests

--- src/main.cu
+++ src/main.cu
@@ -26,14 +26,14 @@ int main(int argc, char** argv)
 
     // Allocate image on host, for CPU specific tasks
     {
-        h_img_1 = load_image(argv[1], &width, &height, &n_channels);
+        h_img_1 = load_image(argv[1], &width, &height, &n_channels, false);
         if (h_img_1 == nullptr)
         {
             spdlog::error("Could not find image {}", argv[1]);
             return -1;
         }
 
-        h_img_2 = load_image(argv[2], &width_, &height_, &n_channels_);
+        h_img_2 = load_image(argv[2], &width_, &height_, &n_channels_, false);
         if (h_img_2 == nullptr)
         {
             spdlog::error("Could not find image {}", argv[1]);
@@ -88,19 +88,26 @@ int main(int argc, char** argv)
                      argv[1], argv[2], width, height, n_channels);
     }
 
+    const char* image_1 = argv[1];
+    const char* image_2 = argv[2];
+
     // Running the tests
 
     // CPU Tests
     test_open_CPU(h_img_1, h_img_2, width, height);
-    test_grayscale_CPU(h_img_1, width, height, n_channels);
-    test_conv_2D_CPU(h_img_1, width, height, n_channels);
-    test_diff_CPU(h_img_1, h_img_2, width, height, n_channels);
+    test_grayscale_CPU(image_1, "CPU_out_gray_1.jpeg");
+    test_grayscale_CPU(image_2, "CPU_out_gray_2.jpeg");
+    test_conv_2D_CPU(image_1, "CPU_out_conv_1.jpeg");
+    test_conv_2D_CPU(image_2, "CPU_out_conv_2.jpeg");
+    test_diff_CPU();
 
     // GPU Tests
     test_open_GPU(d_img_1, d_img_2, width, height, pitch);
-    test_grayscale_GPU(d_img_1, width, height, n_channels, pitch);
-    test_conv_2D_GPU(d_img_1, width, height, n_channels, pitch);
-    test_diff_GPU(d_img_1, d_img_2, width, height, n_channels, pitch);
+    test_grayscale_GPU(image_1, "GPU_out_gray_1.jpeg");
+    test_grayscale_GPU(image_2, "GPU_out_gray_2.jpeg");
+    test_conv_2D_GPU(image_1, "GPU_out_conv_1.jpeg");
+    test_conv_2D_GPU(image_2, "GPU_out_conv_2.jpeg");
+    test_diff_GPU();
 
     free(h_img_1);
     free(h_img_2);
--- src/test.hpp
+++ src/test.hpp
@@ -5,13 +5,13 @@
 void test_open_CPU(unsigned char* h_img_1, unsigned char* h_img_2, int width, int height);
 void test_open_GPU(unsigned char* d_img_1, unsigned char* d_img_2, int width, int height, int pitch);
 
-void test_grayscale_CPU(unsigned char* h_img, int width, int height, int n_channels);
-void test_grayscale_GPU(unsigned char* d_img, int width, int height, int n_channels, int pitch);
+void test_grayscale_CPU(const char* input, const char* output);
+void test_grayscale_GPU(const char* input, const char* output);
 
-void test_conv_2D_CPU(unsigned char* h_img, int width, int height, int n_channels);
-void test_conv_2D_GPU(unsigned char* d_img, int width, int height, int n_channels, int pitch);
+void test_conv_2D_CPU(const char* input, const char* output);
+void test_conv_2D_GPU(const char* input, const char* output);
 
-void test_diff_CPU(unsigned char* h_img_1, unsigned char* h_img_2, int width, int height, int n_channels);
-void test_diff_GPU(unsigned char* d_img_1, unsigned char* d_img_2, int width, int height, int n_channels, int pitch);
+void test_diff_CPU();
+void test_diff_GPU();
 
 #endif
