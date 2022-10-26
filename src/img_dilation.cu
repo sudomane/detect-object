fde9a9a76c0facf3b6f92b032e8c13bc721d3e00
philippe.bouchet <philippe.bouchet@epita.fr>
2022-10-26 14:10:33 +0200

    Started erosion and dilation

---
+++ src/img_dilation.cu
@@ -0,0 +1,9 @@
+#include "img_operations.hpp"
+
+namespace CPU
+{
+void dilation(const u_char* src, u_char* dst, int width, int height)
+{
+
+}
+} // namespace CPU
--- src/img_operations.hpp
+++ src/img_operations.hpp
@@ -13,6 +13,8 @@ void to_grayscale(const u_char* src, u_char* dst, int width, int height, int n_c
 void conv_2D(const u_char* src, u_char* dst, int width, int height);
 void morph(const u_char* src, u_char* dst, int width, int height, int kernel_size, bool minimum);
 void compute_difference(const u_char* img_1, const u_char* img_2, u_char* dst, int width, int height);
+void erosion(const u_char* src, u_char* dst, int width, int height);
+void dilation(const u_char* src, u_char* dst, int width int height);
 }; // namespace CPU
 
 namespace GPU
@@ -23,6 +25,8 @@ __global__ void conv_2D(const u_char* src, u_char* dst, int width, int height,
                         size_t pitch);
 __global__ void compute_difference(const u_char* img_1, const u_char* img_2, u_char* dst,
                                    int width, int height, int pitch);
+__global__ void erosion(const u_char* src, u_char* dst, int width, int height, int pitch);
+__global__ void dilation(const u_char* src, u_char* dst, int width, int height, int pitch);
 }; // namespace GPU
 
 #endif // IMG_OPERATIONS_HPP
